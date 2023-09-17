a1df6ae08a4337e8ffa4f20e9c7c5e566fc359c1
Claudio Fritzen <sdfritzen96@gmail.com>
2023-09-17 23:01:31 +0100

    first commit

---
+++ aula_14.py
@@ -0,0 +1,30 @@
+""" for c in range (1,4):
+    n = int(input('Digite um numero: ' ))
+    print(c)
+print('FIM')
+
+ """
+
+
+""" c = 1
+
+while c < 10:
+    print(c)
+    c = c +1
+print(c) """
+
+""" n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('FIM') """
+
+""" r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N]')).upper()
+print('FIM') """
+
+n = 1 
+while n != 0:
+    n = int(input('Digite um numero: '))
+print()
