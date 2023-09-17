aa2b02c3f629341b6362d3ec164cf2df09a69728
Claudio Fritzen <sdfritzen96@gmail.com>
2023-09-17 23:09:23 +0100

    descobrindo quantos numeros pares e impares

--- aula_14.py
+++ aula_14.py
@@ -24,7 +24,27 @@ while r == 'S':
     r = str(input('Quer continuar? [S/N]')).upper()
 print('FIM') """
 
-n = 1 
+""" n = 1 
 while n != 0:
     n = int(input('Digite um numero: '))
 print()
+ """
+
+""" 
+ quero saber quantos numeros eram pares e impares
+"""
+
+p = 0
+i = 0
+n = 1
+while n != 0:
+    n = int(input('Digite um numero: '))
+
+    if n !=0:
+
+        if n % 2 == 0:
+            p += 1
+        else:
+            i += 1
+
+print(f' voce digitou {p} numeros pares e {i} numeros impares')
