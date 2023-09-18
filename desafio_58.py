c30bf29513d958161e4bc27a7bff498713856241
Claudio Fritzen <sdfritzen96@gmail.com>
2023-09-18 23:50:19 +0100

    tentando resolver o problema

--- desafio_58.py
+++ desafio_58.py
@@ -4,4 +4,12 @@ pensar em um numero entre 0 e 10. Só que agora o
 jogador vai tentar adivinhar até acertar, mostrando 
 no final quantos palpites foram necessario para vencer
 '''
-
+import random
+inicio = 0
+tentativas = 0
+usuario = 1
+while usuario != 0:
+    usuario = int(input('Digite um numero de 1 a 10: '))
+    for sorteio in range(1,11):
+        print(sorteio)
+print('Fim do programa!!')
